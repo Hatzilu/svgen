e28da2f688a8570536a2e3ecade8bd73cad5003c
Hatzilu <itamar246@gmail.com>
2024-06-13 20:37:36 +0300

    progress

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 import { readFileSync, readdirSync, writeFileSync } from "node:fs";
 import path from 'node:path';
-function main() {
-    var svgPath = process.argv[process.argv.findIndex(function (a) { return a === '-p'; }) + 1];
-    var outputPath = process.argv[process.argv.findIndex(function (a) { return a === '-o'; }) + 1];
-    var p = path.resolve(svgPath);
-    var files = readdirSync(p);
+/**
+ * Builds the output file and returns it as a buffer.
+ * @param files file paths read from an SVG directory
+ * @param p - svg directory path
+ * @returns {Buffer} concated buffer
+ */
+export function buildFileString(files, p) {
     var typesBuf = "import { ComponentPropsWithRef } from \"react\"; \r\n\r\ntype Props = ComponentPropsWithRef<'svg'> & {\n        kind:";
     var componentBuf = "export function Icon ({kind, ...props}: Props) {\n    switch (kind) {";
     // writeFile("./output.tsx", )
@@ -18,6 +20,20 @@ function main() {
     });
     typesBuf += "\n};\n\n";
     componentBuf += "\r\n\t\tdefault: return null;\r\n\t};\r\n};";
-    writeFileSync(outputPath, Buffer.from(typesBuf + componentBuf, 'utf-8'));
+    return Buffer.from(typesBuf + componentBuf, 'utf-8');
+}
+function main() {
+    var svgDir = process.argv[process.argv.findIndex(function (a) { return a === '-p'; }) + 1];
+    if (!svgDir) {
+        throw new Error("No svg directory specified, use -p <path>");
+    }
+    var outputPath = process.argv[process.argv.findIndex(function (a) { return a === '-o'; }) + 1];
+    if (!outputPath) {
+        throw new Error("No output specified, use -o <path>");
+    }
+    var p = path.resolve(svgDir);
+    var files = readdirSync(p);
+    var buf = buildFileString(files, p);
+    writeFileSync(outputPath, buf);
 }
 main();
--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,13 @@
 import { readFileSync, readdirSync, writeFileSync } from "node:fs";
 import path from 'node:path';
 
-function main(): void {
-    const svgDir = process.argv[process.argv.findIndex(a => a === '-p')+1];
-    if (!svgDir) {
-        throw new Error("No svg directory specified, use -p <path>")
-    }
-    const outputPath = process.argv[process.argv.findIndex(a => a === '-o')+1];
-    if (!outputPath) {
-        throw new Error("No output specified, use -o <path>")
-    }
-    const p = path.resolve(svgDir);
-
-    const files = readdirSync(p)
-    
+/**
+ * Builds the output file and returns it as a buffer.
+ * @param files file paths read from an SVG directory
+ * @param p - svg directory path
+ * @returns {Buffer} concated buffer
+ */
+export function buildFileString(files: string[], p: string) {
     let typesBuf = `import { ComponentPropsWithRef } from "react"; \r\n\r\ntype Props = ComponentPropsWithRef<'svg'> & {
         kind:`;
     let componentBuf = `export function Icon ({kind, ...props}: Props) {
@@ -31,8 +25,30 @@ function main(): void {
     });
     typesBuf += "\n};\n\n"
     componentBuf += "\r\n\t\tdefault: return null;\r\n\t};\r\n};";
+
+    return Buffer.from(typesBuf+componentBuf, 'utf-8');
+}
+
+function main(): void {
+    const svgDir = process.argv[process.argv.findIndex(a => a === '-p')+1];
+
+    if (!svgDir) {
+        throw new Error("No svg directory specified, use -p <path>")
+    }
+    const outputPath = process.argv[process.argv.findIndex(a => a === '-o')+1];
+
+    if (!outputPath) {
+        throw new Error("No output specified, use -o <path>")
+    }
+
+    const p = path.resolve(svgDir);
+
+    const files = readdirSync(p)
     
-    writeFileSync(outputPath, Buffer.from(typesBuf+componentBuf, 'utf-8'));
+
+    const buf = buildFileString(files, p);
+    
+    writeFileSync(outputPath, buf);
 }
 
 main();
