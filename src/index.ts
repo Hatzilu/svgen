e8cd27aa8411c9453f06415a751a39eba50e5be5
Hatzilu <itamar246@gmail.com>
2024-06-13 20:20:43 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
-import { readFile, readFileSync, readdirSync, writeFile, writeFileSync } from "node:fs";
+#!/usr/bin/env node
+
+import { readFileSync, readdirSync, writeFileSync } from "node:fs";
 import path from 'node:path';
 
 function main(): void {
-    console.log(process.argv);
     const svgPath = process.argv[process.argv.findIndex(a => a === '-p')+1];
+    const outputPath = process.argv[process.argv.findIndex(a => a === '-o')+1];
     
-    console.log({svgPath});
     const p = path.resolve(svgPath);
-    console.log({p});
 
     const files = readdirSync(p)
-    console.log(files);
     
     let typesBuf = `import { ComponentPropsWithRef } from "react"; \r\n\r\ntype Props = ComponentPropsWithRef<'svg'> & {
         kind:`;
@@ -21,16 +20,14 @@ function main(): void {
         const name = f.split('.')[0];
         const fullPath = path.join(p,f);
         const contents = readFileSync(fullPath);
+
         typesBuf += `\r\n\t\t| "${name}"`;
         componentBuf += `\r\n\t\tcase "${name}": return (${contents});`;
-        console.log(`case "${name}": return (...);`);
-        
     });
     typesBuf += "\n};\n\n"
     componentBuf += "\r\n\t\tdefault: return null;\r\n\t};\r\n};";
-    // console.log({typesBuf});
     
-    writeFileSync('./output.tsx', Buffer.from(typesBuf+componentBuf, 'utf-8'));
+    writeFileSync(outputPath, Buffer.from(typesBuf+componentBuf, 'utf-8'));
 }
 
 main();
